07805208ae27090f36af60b6c0f1f03f8e30ce17
Joseph Lee <josephcw.lee@quincus.com>
2022-09-25 04:43:32 +0800

    Include Redis

--- note-api/core/config/database.py
+++ note-api/core/config/database.py
@@ -1,10 +1,13 @@
 import motor.motor_asyncio
 from beanie import init_beanie
 import aioredis
+from pydantic_aioredis import RedisConfig
+from pydantic_aioredis import Store
 
 from core.config import settings
 from core.models.note import Note
 from core.models.user import User
+from core.models.auth import TokenData
 
 mongo_uri = "mongodb+srv://{user}:{password}@{host}/{db}".format(
     user=settings.DB_USER,
@@ -37,8 +40,19 @@ async def redis_pool(db: int = 0):
     """
 
     """
-    redis = await aioredis.from_url(
-        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}?encoding=utf-8", 
-        password=settings.REDIS_PASS
+    # redis = await aioredis.from_url(
+    #     f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}?encoding=utf-8", 
+    #     password=settings.REDIS_PASS
+    # )
+
+    # Redisconfig. Change this configuration to match your redis server
+    redis_config = RedisConfig(
+        db=5, host=settings.REDIS_HOST, password=settings.REDIS_PASS, port=settings.REDIS_PORT
     )
-    return redis
+
+    # Create the store and register your models
+    store = Store(
+        name="note-auth", redis_config=redis_config, life_span_in_seconds=settings.JWT_EXPIRY
+    )
+    store.register_model(TokenData)
+    
