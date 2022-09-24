e776918230a41501b63da2969806877cedd65c54
Joseph Lee <josephcw.lee@quincus.com>
2022-09-24 10:38:24 +0800

    add redis

--- note-api/core/config/database.py
+++ note-api/core/config/database.py
@@ -1,31 +1,44 @@
 import motor.motor_asyncio
 from beanie import init_beanie
+import aioredis
 
-from core.config.settings import db_host, db_name, db_pass, db_user, db_socket_timeout, db_connect_timeout
+from core.config import settings
 from core.models.note import Note
 from core.models.user import User
 
 mongo_uri = "mongodb+srv://{user}:{password}@{host}/{db}".format(
-    user=db_user,
-    password=db_pass,
-    host=db_host,
-    db=db_name
+    user=settings.DB_USER,
+    password=settings.DB_PASS,
+    host=settings.DB_HOST,
+    db=settings.DB_NAME
 )
 
 client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri,
-                                                    connectTimeoutMS=db_connect_timeout,
-                                                    socketTimeoutMS=db_socket_timeout,
-                                                    w='majority',
-                                                    retryWrites=True,
-                                                    readPreference='secondary')
+                                                connectTimeoutMS=settings.DB_TIMEOUT,
+                                                socketTimeoutMS=settings.DB_TIMEOUT,
+                                                w='majority',
+                                                retryWrites=True,
+                                                readPreference='secondary')
 
-db = client[db_name]
+db = client[settings.DB_NAME]
+
 
 async def init_db(db):
-    
+
     await init_beanie(database=db,
-        document_models=[
-            Note,
-            User
-        ]
+                      document_models=[
+                          Note,
+                          User
+                      ]
+                      )
+
+
+async def redis_pool(db: int = 0):
+    """
+
+    """
+    redis = await aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}?encoding=utf-8", 
+        password=settings.REDIS_PASS
     )
+    return redis
