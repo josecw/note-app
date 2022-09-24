7740d5435740e82c2a8660fc9975a45a3f07465b
Joseph Lee <josephcw.lee@quincus.com>
2022-09-25 04:44:06 +0800

    Include Redis

--- note-api/main.py
+++ note-api/main.py
@@ -1,17 +1,35 @@
-from beanie import init_beanie
 from fastapi import FastAPI
+import uvicorn
 
 from core.routes.v1.api import api_router as v1_api
-from core.config.database import init_db, db
+from core.config.database import init_db, db, redis_pool
+from core.config import settings
+
 app = FastAPI()
 app.db = db
+
 
 @app.on_event("startup")
 async def on_startup():
     await init_db(app.db)
+    app.state.redis = await redis_pool()
+
+
+# @app.on_event("shutdown")
+# async def close_redis():
+#     await app.state.redis.close()
 
 app.include_router(v1_api, prefix='/v1')
 
+
 @app.get("/", include_in_schema=False)
-async def read_root() -> dict:
+async def root() -> dict:
     return {"message": "Note-App is live"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",
+                host=settings.HOST,
+                reload=settings.DEBUG_MODE,
+                port=settings.PORT,
+                log_level=settings.LOG_LEVEL
+                )
